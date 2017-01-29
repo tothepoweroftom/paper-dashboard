463a53f46f2225d9282718aae8af9fdee1f3c833
Tom Power <tom.power@mobgen.com>
2017-01-29 14:57:48 +0100

    Add

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -21,9 +21,9 @@ demo = {
         var dataSales = {
           labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
           series: [
-             [287, 385, 490, 562, 594, 626, 698, 895, 952],
-            [67, 152, 193, 240, 387, 435, 535, 642, 744],
-            [23, 113, 67, 108, 190, 239, 307, 410, 410]
+             [1000, 385, 490, 562, 594, 626, 698, 895, 952],
+            [67, 152, 193, 199, 387, 435, 535, 642, 744],
+            [23, 600, 67, 108, 190, 239, 307, 410, 410]
           ]
         };
 
@@ -37,10 +37,10 @@ demo = {
             showGrid: false,
           },
           lineSmooth: Chartist.Interpolation.simple({
-            divisor: 3
+            divisor: 2
           }),
           showLine: true,
-          showPoint: false,
+          showPoint: true,
         };
 
         var responsiveSales = [
